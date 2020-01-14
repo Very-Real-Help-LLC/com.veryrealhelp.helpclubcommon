915b644288a36192c932759e3dc849e9b4755a07
Tanner Netterville <tannern@gmail.com>
2020-01-14 15:36:42 -0600

    add existing light modification file

---
+++ Runtime/LightModificationFile.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace VeryRealHelp.HelpClub {
+
+    [CreateAssetMenu(fileName = "LightModData", menuName = "VRH/LightModificationFile", order = 1)]
+    public class LightModificationFile : ScriptableObject
+    {
+        public Material skyboxMat;
+        [ColorUsage(true, true)]
+        public Color skyColor;
+        [ColorUsage(true, true)]
+        public Color equatorColor;
+        [ColorUsage(true, true)]
+        public Color groundColor;
+    }
+    
+}
