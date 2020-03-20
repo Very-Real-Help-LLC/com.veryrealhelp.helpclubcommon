64215a48c82ff283d4d9b582f34c376048e37214
Tanner Netterville <tannern@gmail.com>
2020-03-20 12:40:16 -0500

    0.0.11 OSX bundle build script and menu options

--- Editor/BuildScript.cs
+++ Editor/BuildScript.cs
@@ -9,10 +9,12 @@ namespace VRH
     {
         public static BuildAssetBundleOptions bundleOptions = BuildAssetBundleOptions.StrictMode & BuildAssetBundleOptions.ChunkBasedCompression;
 
+        [MenuItem("VRH/Bundles/Build All")]
         public static void BuildWorldBundles()
         {
             BuildBundlesAndroid();
             BuildBundlesStandaloneWindows();
+            BuildBundlesStandaloneOSX();
         }
 
         public static IEnumerator BuildWorldBundlesCoroutine()
@@ -21,8 +23,11 @@ namespace VRH
             yield return null;
             BuildBundlesStandaloneWindows();
             yield return null;
+            BuildBundlesStandaloneOSX();
+            yield return null;
         }
-
+        
+        [MenuItem("VRH/Bundles/Build Android Bundles")]
         public static void BuildBundlesAndroid()
         {
             Debug.Log("Building Android Bundles...");
@@ -30,11 +35,20 @@ namespace VRH
             Debug.Log("Finished Building Android Bundles");
         }
 
+        [MenuItem("VRH/Bundles/Build Standalone Windows Bundles")]
         public static void BuildBundlesStandaloneWindows()
         {
             Debug.Log("Building Standalone Windows Bundles...");
             BuildPipeline.BuildAssetBundles("AssetBundles/StandaloneWindows", bundleOptions, BuildTarget.StandaloneWindows);
             Debug.Log("Finished Building Standalone Windows Bundles");
+        }
+
+        [MenuItem("VRH/Bundles/Build Standalone OSX Bundles")]
+        public static void BuildBundlesStandaloneOSX()
+        {
+            Debug.Log("Building Standalone OSX Bundles...");
+            BuildPipeline.BuildAssetBundles("AssetBundles/StandaloneOSX", bundleOptions, BuildTarget.StandaloneOSX);
+            Debug.Log("Finished Building Standalone OSX Bundles");
         }
 
     }
