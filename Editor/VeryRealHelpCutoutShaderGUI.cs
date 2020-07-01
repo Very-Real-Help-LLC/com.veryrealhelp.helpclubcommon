849682ddc3d2fcea520481345ac622d57dfdbca2
Tanner Netterville <tannern@gmail.com>
2020-07-01 14:37:07 -0500

    add Cutout shader

---
+++ Editor/VeryRealHelpCutoutShaderGUI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class VeryRealHelpCutoutShaderGUI : ShaderGUI
+{
+    private Texture2D alphaPlot;
+    private MaterialProperty alphaPosition;
+    private MaterialProperty alphaSlope;
+
+    private float lastPosition;
+    private float lastSlope;
+    
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        base.OnGUI(materialEditor, properties);
+        foreach (var item in properties)
+        {
+            if (item.name == "_CutoffPosition")
+            {
+                alphaPosition = item;
+            }
+            else if (item.name == "_CutoffSlope")
+            {
+                alphaSlope = item;
+            }
+        }
+        bool redraw = false;
+        if (lastPosition != alphaPosition.floatValue)
+        {
+            lastPosition = alphaPosition.floatValue;
+            redraw = true;
+        }
+        if (lastSlope != alphaSlope.floatValue)
+        {
+            lastSlope = alphaSlope.floatValue;
+            redraw = true;
+        }
+        if (redraw)
+        {
+            UpdateAlphaPlot();
+        }
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        EditorGUILayout.LabelField("Alpha Cutoff Curve");
+        EditorGUILayout.LabelField("A steeper curve results in harsher edges.", EditorStyles.miniLabel);
+        EditorGUILayout.EndVertical();
+        GUILayout.Box(alphaPlot);
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void UpdateAlphaPlot()
+    {
+        float interp(float position, float slope, float value)
+        {
+            float c = 2f / (1f - slope) - 1f;
+            float f(float x, float n) => Mathf.Pow(x, c) / Mathf.Pow(n, c - 1);
+            return value < position ? f(value, position) : (1 - f(1 - value, 1 - position));
+        }
+        if (alphaPlot == null)
+        {
+            alphaPlot = new Texture2D(64, 64, TextureFormat.RGBA32, false);
+        }
+        float t;
+        for (int x = 0; x < alphaPlot.width; x++)
+        {
+            t = x / (float)alphaPlot.width;
+            for (int y = 0; y < alphaPlot.height; y++)
+            {
+                var z = interp(alphaPosition.floatValue, alphaSlope.floatValue, t);
+                var color = z < (y / (float)alphaPlot.height) ? Color.white : Color.black;
+                var refColor = x < y ? Color.white : Color.black;
+                alphaPlot.SetPixel(x, y, Color.Lerp(color, refColor, 0.25f));
+            }
+        }
+        alphaPlot.Apply();
+    }
+}
---
+++ Shaders/Lit/Cutout.shader
@@ -0,0 +1,73 @@
+﻿Shader "VeryRealHelp/Lit/Cutout"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+		_LightIntensity("Light Intensity", Range(0,2)) = 1
+		_MainTex("Albedo (RGB)", 2D) = "white" {}
+		_ShinyColor("Shinyness Color", Color) = (0,0,0,1)
+		_ShinyTex("Shinyness", 2D) = "white" {}
+		[HDR]
+		_EmissionColor("Emission Color", Color) = (0,0,0,0)
+		_EmissionTex("Emission", 2D) = "white" {}
+		_CutoffPosition("Alpha Cutoff Position", Range(0,1)) = 0.5
+		_CutoffSlope("Alpha Cutoff Slope", Range(-0.9,0.9)) = 0.333
+	}
+	SubShader
+	{
+		Tags { "RenderType" = "Opaque" }
+		LOD 200
+		AlphaToMask On
+
+		CGPROGRAM
+		#pragma surface surf StandardSpecular noshadow noshadowmask nodynlightmap nodirlightmap exclude_path:deferred alphatest:_Cutoff
+		#pragma target 3.5
+
+		sampler2D _MainTex;
+		half _LightIntensity;
+		half4 _ShinyColor;
+		sampler2D _ShinyTex;
+		half4 _EmissionColor;
+		sampler2D _EmissionTex;
+		half _CutoffPosition;
+		half _CutoffSlope;
+
+		struct Input
+		{
+			float2 uv_MainTex;
+		};
+
+		fixed4 _Color;
+
+		// Add instancing support for this shader. You need to check 'Enable Instancing' on materials that use the shader.
+		// See https://docs.unity3d.com/Manual/GPUInstancing.html for more information about instancing.
+		// #pragma instancing_options assumeuniformscaling
+		UNITY_INSTANCING_BUFFER_START(Props)
+			// put more per-instance properties here
+		UNITY_INSTANCING_BUFFER_END(Props)
+
+		half interpF(half c, half x, half n) {
+			return pow(x, c) / pow(n, c - 1);
+		}
+		half interp(half position, half slope, half value) {
+			half c = 2 / (1 - slope) - 1;
+			half f = interpF(c, value, position);
+			half g = 1 - interpF(c, 1 - value, 1 - position);
+			return lerp(f, g, floor(position + 0.5));
+		}
+
+		void surf(Input IN, inout SurfaceOutputStandardSpecular o)
+		{
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+			o.Albedo = c.rgb;
+			o.Alpha = interp(_CutoffPosition, _CutoffSlope, c.a);
+			fixed4 shiny = lerp(tex2D(_ShinyTex, IN.uv_MainTex), _ShinyColor, _ShinyColor.a);
+			o.Smoothness = shiny.r * 0.5;
+			o.Specular = c.rgb * shiny.rgb;
+			o.Emission = lerp(half3(0,0,0), tex2D(_EmissionTex, IN.uv_MainTex).rgb * _EmissionColor.rgb, _EmissionColor.a);
+		}
+		ENDCG
+	}
+	CustomEditor "VeryRealHelpCutoutShaderGUI"
+	FallBack "Diffuse"
+}
