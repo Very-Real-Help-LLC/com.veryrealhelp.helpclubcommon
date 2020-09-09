2965cf5adaa2f9acb2b2f607ede4ecf2102b912a
Rangarz <matttppp@gmail.com>
2020-09-09 21:24:54 +0200

    Revert "Added complex multipart props"
    This reverts commit 0ad28f27aa1fb451dd7103cdab3493ead45e7466.

--- Runtime/Props/PropDefinition.cs
+++ Runtime/Props/PropDefinition.cs
@@ -5,10 +5,7 @@ namespace VeryRealHelp.HelpClubCommon.Prop
     [CreateAssetMenu(fileName = "Prop Definition", menuName = "Very Real Help/Prop/PropDefinition")]
     public class PropDefinition : ScriptableObject
     {
-        public PropType Type;
-        public PropFollowType FollowType;
-        public PropComplexity Complexity;
+        public bool IsUsable;
         public GameObject Prefab;
-        public Vector3 Offset;
     }
 }
