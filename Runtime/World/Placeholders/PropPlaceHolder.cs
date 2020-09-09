d7253d9c7b821c573dd9c7c266cb6f7cbd3a6ba7
Rangarz <matttppp@gmail.com>
2020-09-09 20:47:22 +0200

    World Props: Prob Place Holders

---
+++ Runtime/World/Placeholders/PropPlaceHolder.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace VeryRealHelp.HelpClubCommon.World
+{
+    public class PropPlaceHolder : Placeholder
+    {
+        public GameObject PropPrefab;
+    }
+}
