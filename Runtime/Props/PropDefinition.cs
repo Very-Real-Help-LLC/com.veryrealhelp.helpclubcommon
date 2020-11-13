d5c15bf49f29a0ca3181e5e62b07689dbc6742c4
Rangarz <matttppp@gmail.com>
2020-11-14 00:03:29 +0200

    Prop definition layer

--- Runtime/Props/PropDefinition.cs
+++ Runtime/Props/PropDefinition.cs
@@ -5,7 +5,31 @@ namespace VeryRealHelp.HelpClubCommon.Prop
     [CreateAssetMenu(fileName = "Prop Definition", menuName = "Very Real Help/Prop/PropDefinition")]
     public class PropDefinition : ScriptableObject
     {
-        public bool IsUsable;
+        public enum PropType
+        {
+            Default = 1,
+            UnstealableTest = 2,
+            UsableTest = 3,
+            TextProp = 4,
+            HelperProp = 5,
+            TextHelperProp = 6
+        }
+        public enum PropFollowType
+        {
+            FollowOrientation = 1,
+            FixedOrientation = 2 
+        }
+        public enum PropCategory
+        {
+            Game = 1,
+            HelperOnly = 2,
+            SMART = 3,
+            DBT = 4,
+            CBT = 5
+        }
+        public PropType Type;
+        public PropFollowType FollowType;
+        public PropCategory Category;
         public GameObject Prefab;
     }
 }
