f9bb29edf87a8825509e70af104f2c21e67e8a00
= <matttppp@gmail.com>
2021-01-14 23:28:21 +0200

    Update BatchBuild.cs

--- Editor/Automation/BatchBuild.cs
+++ Editor/Automation/BatchBuild.cs
@@ -33,8 +33,18 @@ namespace VeryRealHelp.HelpClubCommon.Editor.Automation
                     targets.Add(BuildTarget.StandaloneOSX);
                 if (args.ContainsKey("win"))
                     targets.Add(BuildTarget.StandaloneWindows);
+
+#if UNITY_EDITOR_OSX
+                 if (args.ContainsKey("ios"))
+                    targets.Add(BuildTarget.iOS);
+
+                 if (targets.Count == 0)  // if no targets specified, target all
+                    targets = new HashSet<BuildTarget> { BuildTarget.Android, BuildTarget.StandaloneOSX, BuildTarget.StandaloneWindows, BuildTarget.iOS };
+#else
+
                 if (targets.Count == 0)  // if no targets specified, target all
                     targets = new HashSet<BuildTarget> { BuildTarget.Android, BuildTarget.StandaloneOSX, BuildTarget.StandaloneWindows };
+#endif
             }
 
             public string BuildPath => $"{buildRoot}/{buildName}";
