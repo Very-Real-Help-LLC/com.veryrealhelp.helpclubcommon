d2f55da643d56a3f5eab51d90e51b3dc728b5067
Tanner Netterville <tannern@gmail.com>
2021-11-11 17:44:54 -0600

    Add PlayAudioClipBehavior to enable animation triggered audio events

---
+++ Runtime/Audio/PlayAudioClipBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayAudioClipBehavior : MonoBehaviour
+{
+    [Header("Optional")]
+    public AudioSource audioSource;
+
+    public void Start()
+    {
+        if (audioSource == null)
+        {
+            audioSource = GetComponent<AudioSource>();
+            if (audioSource == null)
+            {
+                audioSource = gameObject.AddComponent<AudioSource>();
+            }
+        }
+    }
+
+    public void PlayAudioClip(AudioClip clip) => audioSource.PlayOneShot(clip);
+}
