5802a4f8b04fc159f3c1c8f1088825ffb5b9929f
karim stock <karim@veryrealhelp.com>
2022-05-10 15:56:04 +0200

    Added renderers and materials to ACS
    - Added renderers and materials to AvatarComponentSpecification

--- Runtime/Avatar/AvatarComponentSpecification.cs
+++ Runtime/Avatar/AvatarComponentSpecification.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace VeryRealHelp.HelpClubCommon.Avatar
 {
@@ -14,5 +15,36 @@ namespace VeryRealHelp.HelpClubCommon.Avatar
         public AvatarComponentSlot slot;
         public SlotTransformOverride[] slotTransformOverrides;
         public Sprite thumbnail;
+
+        private Renderer[] renderers;
+        private Dictionary<Renderer, Material> cachedMaterials;
+
+        public Renderer[] Renderers()
+        {
+            if(renderers == null)
+            {
+                renderers = GetComponentsInChildren<Renderer>();
+                if (cachedMaterials == null)
+                {
+                    cachedMaterials = new Dictionary<Renderer, Material>();
+                    for(int i = 0; i < renderers.Length; i++)
+                    {
+                        cachedMaterials.Add(renderers[i], renderers[i].material);
+                    }
+                }
+            }
+            return renderers;
+        }
+        public void ApplyCachedMaterial()
+        {
+            if(cachedMaterials == null || cachedMaterials.Count == 0)
+            {
+                return;
+            }
+            foreach(KeyValuePair<Renderer, Material> cachedRendMaterials in cachedMaterials)
+            {
+                cachedRendMaterials.Key.material = cachedRendMaterials.Value;
+            }
+        }
     }
 }
