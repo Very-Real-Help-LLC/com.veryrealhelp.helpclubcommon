51101790deffd8040c66e54aeb4677850f82a610
karim stock <karim@veryrealhelp.com>
2022-05-10 18:04:05 +0200

    Added MirrorPlaceholder

---
+++ Runtime/World/Placeholders/MirrorPlaceholder.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace VeryRealHelp.HelpClubCommon.World
+{
+    public class MirrorPlaceholder : Placeholder
+    {
+        private static readonly Vector3 mirrorSize = new Vector3(1.9f, 2.5f, 0.1f);
+
+
+        public void OnDrawGizmos()
+        {
+            if (isActiveAndEnabled)
+            {
+                Gizmos.matrix = transform.localToWorldMatrix;
+                Gizmos.color = Color.magenta;
+                Gizmos.DrawCube(Vector3.zero + mirrorSize.y * 0.5f * Vector3.up, mirrorSize);
+            }
+        }
+    }
+}
