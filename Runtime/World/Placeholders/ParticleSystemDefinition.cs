e2bef590dc4ec0196abde465dc7f5b785bc8f544
karim stock <32227711+khstock@users.noreply.github.com>
2022-05-19 09:39:10 -0700

    Feature/particle system (#27)
    * Added world builder tools custom ParticleSystem
    
    - Added world builder tools custom ParticleSystem
    - Added editor script for particle system
    - Added mask to mirror
    - Adjusted editor previews for mirror and particlesystem
    
    * Adjusted particle system gizmos
    
    Co-authored-by: karim stock <karim@veryrealhelp.com>

---
+++ Runtime/World/Placeholders/ParticleSystemDefinition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace VeryRealHelp.HelpClubCommon.Particles
+{
+    public class ParticleSystemDefinition
+    {
+        public enum ParticleType
+        {
+            None = 0,
+            Prefab = 1,
+            Text = 2,
+        }
+    }
+}
---
+++ Runtime/World/Placeholders/ParticleSystemPlaceholder.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using VeryRealHelp.HelpClubCommon.Particles;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace VeryRealHelp.HelpClubCommon.World
+{
+
+    public class ParticleSystemPlaceholder : Placeholder
+    {
+        //generic
+        public ParticleSystemDefinition.ParticleType particleType;
+        public GameObject particlePrefab;
+
+        //ps settings
+        public float particleLifetime = 2f;
+        public float minScale = 0.5f;
+        public float maxScale = 1.5f;
+        public Vector3 spawnAreaSize = new Vector3(3f, 1.55f, 2f);
+
+        //text related
+        public string[] textMessages;
+        public float textFadeDuration = 2f;
+        public Vector3 textScrollingOffset = new Vector3(0f, 0.1f, 0.15f);
+
+        public void OnDrawGizmos()
+        {
+            if (isActiveAndEnabled)
+            {
+                Gizmos.matrix = transform.localToWorldMatrix;
+                Color transparentAreaColor = Color.blue;
+                transparentAreaColor.a = 0.2f;
+                Gizmos.color = transparentAreaColor;
+                Gizmos.DrawCube(Vector3.zero, spawnAreaSize);
+            }
+        }
+    }
+}
