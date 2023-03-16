50b51b93e6473d6003099b76e929a9b0f3a86e50
Tanner Netterville <tannern@gmail.com>
2023-03-15 22:48:37 -0500

    add PlatformSpecificObject

---
+++ Runtime/World/PlatformSpecificObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpecificObject : MonoBehaviour
+{
+    [Flags] public enum Platform { None = 0, XR = 1, PC = 1<<1, Touch = 1<<2 }
+
+    public Platform platforms;
+
+    public static event Action<PlatformSpecificObject> OnAwake;
+
+    private void Awake()
+    {
+        OnAwake?.Invoke(this);
+    }
+}
